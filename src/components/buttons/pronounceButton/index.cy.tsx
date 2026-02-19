0f38534762f3604dc502155a5d87070ce1181dbd
DaniloAlvesantos <daniloasan.itapira@gmail.com>
2026-02-19 13:45:23 -0300

    fix: env config test

--- src/components/buttons/pronounceButton/index.cy.tsx
+++ src/components/buttons/pronounceButton/index.cy.tsx
@@ -1,17 +1,18 @@
 import { PronounceButton } from "./index";
 
-const isCI = Cypress.env("CI") || false;
+const isCI = process.env.CI === 'true' || !!process.env.GITHUB_ACTIONS;
 
 describe("Speech Synthesis Testing", () => {
-  (isCI ? it.skip : it)(
-    "should call speechSynthesis.speak when the button is clicked",
-    () => {
-      cy.mount(<PronounceButton word="Ephemeral" />);
-      const speakSpy = cy.spy(window.speechSynthesis, "speak").as("speakSpy");
+  const testFunc = isCI ? it.skip : it;
 
-      cy.get("button").click();
-      cy.get("@speakSpy").should("have.been.called");
-    }
-  );
+  testFunc("should call speechSynthesis.speak when the button is clicked", () => {
+    cy.mount(<PronounceButton word="Ephemeral" />);
+    
+    cy.window().then((win) => {
+      cy.spy(win.speechSynthesis, "speak").as("speakSpy");
+    });
+
+    cy.get("button").click();
+    cy.get("@speakSpy").should("have.been.called");
+  });
 });
-
