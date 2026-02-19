09b6bf0cbd9a41879a3ef0ab645e1e5e13f51172
DaniloAlvesantos <daniloasan.itapira@gmail.com>
2026-02-19 12:51:31 -0300

    fix: workflow and pronounce button test

--- src/components/buttons/pronounceButton/index.cy.tsx
+++ src/components/buttons/pronounceButton/index.cy.tsx
@@ -3,7 +3,10 @@ import { PronounceButton } from "./index";
 describe("Speech Synthesis Testing", () => {
   it("should call speechSynthesis.speak when the button is clicked", () => {
     cy.mount(<PronounceButton word="Ephemeral" />);
-    const speakSpy = cy.spy(window.speechSynthesis, "speak").as("speakSpy");
+
+    cy.window().then((win) => {
+      cy.stub(win.speechSynthesis, "speak").as("speakSpy");
+    });
 
     cy.get("button").click();
 
